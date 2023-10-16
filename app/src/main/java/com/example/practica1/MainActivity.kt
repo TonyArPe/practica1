21edac9f80284b3e102839b92e2ff1120c3eadfe
TonyArPe <tonyaragonp@gmail.com>
2023-10-16 10:57:07 +0200

    Version 1.0 Creacion de la variable TAG

---
+++ app/src/main/java/com/example/practica1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.practica1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    private val TAG = "Pantalla"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
