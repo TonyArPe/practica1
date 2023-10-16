651c12e20194afa9f722983d44a2788072ea3298
TonyArPe <tonyaragonp@gmail.com>
2023-10-16 11:02:17 +0200

    Version 1.1 sobreescripcion del metodo onStart()

--- app/src/main/java/com/example/practica1/MainActivity.kt
+++ app/src/main/java/com/example/practica1/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.practica1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "Pantalla"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Sobreescribimos los metodos principales de onStart onResume
+        //onPause onRestart onStop onDestroy
+
+    }
+    override fun onStart(){
+        super.onStart()
+        Log.d(TAG, "Se llama despues de onCreate()." +
+                " Muestro la pantalla. El usuario aún no puede" +
+                " interaccionar")
+
     }
 }
